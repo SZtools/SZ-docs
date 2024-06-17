3e4a9ddb96f3f3bdffb9637307064bca0d94ba00
PietroFesti <pit.festi@gmail.com>
2024-06-17 17:42:03 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@ extensions = [
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
+    #'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
     'sphinx.ext.viewcode',
     'sphinx_rtd_theme',
@@ -31,16 +31,17 @@ templates_path = ['_templates']
 
 # -- Options for HTML output
 
+html_title = 'Susceptibility Zoning plugin Guide'
+
 html_theme = 'sphinx_rtd_theme'
 
-
 # Add any paths that contain custom static files (such as style sheets) here, relative to this directory.
-html_static_path = ['_static']
+#html_static_path = ['_static']
 
 # Add custom CSS files
-html_css_files = [
+#html_css_files = [
     'custom.css',
-]
+#]
 
 
 # -- Options for the theme
@@ -59,6 +60,8 @@ html_sidebars = {
        'searchbox.html',  # Search box
    ]
 }
+
+# html_use_index = True
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
